8197c63ad9d06f156346e78a68f7d86ed3e9b4c3
River Phillips <riverphillips1@gmail.com>
2024-07-28 10:58:57 +0100

    Add some comments

--- raft/state_machine.go
+++ raft/state_machine.go
@@ -1,6 +1,10 @@
 package raft
 
+// NoOpStateMachine is a state machine that does nothing
+// It's used for testing
 type NoOpStateMachine struct{}
+
+var _ StateMachine = &NoOpStateMachine{}
 
 // Apply returns a result for every command
 func (n *NoOpStateMachine) Apply(command ...Command) []Result {
